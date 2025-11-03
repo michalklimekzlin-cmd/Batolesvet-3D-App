47b55c0de2191eff5f94747020fc61ce7b915b9c
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-03 17:21:58 +0100

    Delete feeds.glyph.js

--- feeds.glyph.js
+++
@@ -1,3 +0,0 @@
-import { viri } from "./viri.mind.js";
-export function saySymbol(sym, meaning=""){ viri.learn("glyph", { signal:sym, value:0.6, note:meaning }); }
-globalThis.VAFI ||= {}; globalThis.VAFI.glyph = { saySymbol };
