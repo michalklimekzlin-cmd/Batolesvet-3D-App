f1e30672d928615ddfed0841fe5c108dd0cc618b
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-03 19:23:59 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// service-worker.js
+const CACHE_NAME = "vaf-cache-v0.31";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css?v=0.31",
+  "./app.js?v=0.31",
+  "./engine.js?v=0.31",
+  "./teams.js?v=0.31",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
