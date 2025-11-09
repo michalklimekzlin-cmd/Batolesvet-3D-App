e9897276ac3152142542603e102ee6340b99043b
michalklimekzlin-cmd <michalklimek.zlin@gmail.com>
2025-11-09 17:27:03 +0100

    Create vaft.apps.js

---
+++ vaft.apps.js
@@ -0,0 +1,16 @@
+// jednoduchý registr appek pro Vivere atque FruiT
+// můžeš sem pak přidávat další moduly
+window.VAFT = window.VAFT || {};
+VAFT.apps = [
+  {
+    name: "Revia",
+    url: "./Revia/"
+  },
+  {
+    name: "Atlas 3I",
+    url: "./3I-ATLAS/"
+  }
+];
+
+// jen info do konzole, že to běží
+console.log("VAFT apps loaded:", VAFT.apps);
